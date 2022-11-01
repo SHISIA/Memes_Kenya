87f9361117084d24321ae22b43ab82657b299a0f
apple <jamesshisia6@gmail.com>
2022-11-01 23:27:42 +0300

    Added Service, Repo and Implementations

---
+++ src/main/java/com/memesKenya/meme/controller/MessageController.java
@@ -0,0 +1,7 @@
+package com.memesKenya.meme.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+}
---
+++ src/main/java/com/memesKenya/meme/model/PostModel.java
@@ -0,0 +1,4 @@
+package com.memesKenya.meme.model;
+
+public class PostModel {
+}
