326d3171eaf74b4ac46937800512a8da672eb252
SHISIA <jamesshisia6@gmail.com>
2022-12-14 09:55:14 +0300

    Account status checkers and setters

--- src/main/java/com/memesKenya/meme/entities/AccountStatus.java
+++ src/main/java/com/memesKenya/meme/entities/AccountStatus.java
@@ -1,9 +1,9 @@
 package com.memesKenya.meme.entities;
 
 public enum AccountStatus {
-    BLOCKED,
-    ACTIVE,
-    ON_HOLD,
-    UNDER_INVESTIGATION,
-    AWAITING_CONFIRMATION
+    BLOCKED,//permanent user account revocation
+    ACTIVE,//active ,usable account
+    ON_HOLD,//held by admin for various reasons mostly punishment for violation on platform rules
+    UNDER_INVESTIGATION,//investigated/under investigation for suspicious behaviour
+    AWAITING_CONFIRMATION //account awaits activation / usability confirmation
 }
