5d514cbd4624b064312f7eb15ece3cfd83a6423e
Jemo <jamesshisia6@gmail.com>
2023-09-10 23:33:18 +0300

    Tidying up code

--- src/main/java/com/memesKenya/meme/MemeApplication.java
+++ src/main/java/com/memesKenya/meme/MemeApplication.java
@@ -3,7 +3,6 @@ package com.memesKenya.meme;
 import com.memesKenya.meme.configs.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableConfigurationProperties(RsaKeyProperties.class)
 
 public class MemeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MemeApplication.class, args);
 	}
