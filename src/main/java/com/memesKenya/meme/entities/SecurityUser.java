13f6ce535dc2b8b69d6092675188582e4f5d2614
Jemo <jamesshisia6@gmail.com>
2023-09-10 23:36:31 +0300

    Used in favor of creating a custom users security database. Might be removed in future to favor a more generalistic and in-one-place  table structure

---
+++ src/main/java/com/memesKenya/meme/entities/SecurityUser.java
@@ -0,0 +1,27 @@
+package com.memesKenya.meme.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+@Table(
+        name = "users"
+)
+@Data
+public class SecurityUser {
+    @Id
+    @Column(
+            nullable = false
+    )
+    private String username;
+    @Column(
+            nullable = false
+    )
+    private String password;
+    private int enabled;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "username_id", referencedColumnName = "username")
+    private Authorities authorities;
+}
