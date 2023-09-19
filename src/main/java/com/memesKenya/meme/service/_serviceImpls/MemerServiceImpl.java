6acd4ecb3761762703e5650dc15054b7068f2c68
Jemo <jamesshisia6@gmail.com>
2023-09-19 19:30:21 +0300

    Save new users, including their authorities/ROLES

--- src/main/java/com/memesKenya/meme/service/_serviceImpls/MemerServiceImpl.java
+++ src/main/java/com/memesKenya/meme/service/_serviceImpls/MemerServiceImpl.java
@@ -1,13 +1,15 @@
 package com.memesKenya.meme.service._serviceImpls;
 
+import com.memesKenya.meme.entities.Authorities;
 import com.memesKenya.meme.entities.Memer;
+import com.memesKenya.meme.entities.SecurityUser;
 import com.memesKenya.meme.model.Person;
+import com.memesKenya.meme.repository.AuthoritiesRepo;
 import com.memesKenya.meme.repository.MemerRepo;
+import com.memesKenya.meme.repository.SecurityUserRepo;
 import com.memesKenya.meme.service._service.MemerService;
-import org.bouncycastle.crypto.generators.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,8 +24,16 @@ import java.util.UUID;
 @Service
 public class MemerServiceImpl implements MemerService {
     @Autowired
+    private AuthoritiesRepo authoritiesRepo;
+    @Autowired
+    private SecurityUserRepo securityUserRepo;
+    @Autowired
     private MemerRepo repo;
     private final PasswordEncoder passwordEncoder;
+
+    private Memer memer;
+    private SecurityUser user;
+    private Authorities authorities;
 
     public MemerServiceImpl(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
@@ -36,15 +46,21 @@ public class MemerServiceImpl implements MemerService {
 
     @Override
     public boolean registerNewMemer(Person person) {
-        Memer memer=new Memer(person.getUserName(),
+         memer=new Memer(person.getUserName(),
                 passwordEncoder.encode(person.getUserPassword()),
                 null, person.getEmailAddress(),
                 person.getFirstName(), person.getSecondName(),
                 "@"+person.getNickName(), person.getPhoneNumber(),
                 person.getAccountStatus());
+         authorities=new Authorities(person.getUserName(),"ROLE_USER",user);
+         user=new SecurityUser(person.getUserName(), passwordEncoder.encode(person.getUserPassword()),
+                1,authorities,memer);
+
         Optional<Memer> memerOptional=Optional.ofNullable(repo.findByNickName(person.getNickName()));
         if (memerOptional.isEmpty()){
             repo.save(memer);
+            authoritiesRepo.save(authorities);
+            securityUserRepo.save(user);
             return true;
         }
         return false;
