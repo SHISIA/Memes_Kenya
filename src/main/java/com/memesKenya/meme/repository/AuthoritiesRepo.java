a9fd3b8e4361c5a358d6a49974e178c8893e5da0
Jemo <jamesshisia6@gmail.com>
2023-09-19 19:29:18 +0300

    Repositories for a new user: allows the user to be used across the app via Spring Security and Memer api

---
+++ src/main/java/com/memesKenya/meme/repository/AuthoritiesRepo.java
@@ -0,0 +1,7 @@
+package com.memesKenya.meme.repository;
+
+import com.memesKenya.meme.entities.Authorities;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthoritiesRepo extends JpaRepository<Authorities, String> {
+}
