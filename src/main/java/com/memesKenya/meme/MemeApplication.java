b04abe460910acfd3332ee7c3bfda958c9f08baf
Jemo <jamesshisia6@gmail.com>
2023-09-28 13:26:20 +0300

    restTemplete bean required for MemerServiceImpl programmatic post and get

--- src/main/java/com/memesKenya/meme/MemeApplication.java
+++ src/main/java/com/memesKenya/meme/MemeApplication.java
@@ -4,7 +4,10 @@ import com.memesKenya.meme.configs.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 //discards Spring security's auto-configuration
 //@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
@@ -16,4 +19,10 @@ public class MemeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MemeApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
+
