c397494b7b147f78d6f7822c218de50368fb7f25
Jemo <jamesshisia6@gmail.com>
2023-09-28 13:23:34 +0300

    rececive auth token

--- src/main/java/com/memesKenya/meme/controller/AuthController.java
+++ src/main/java/com/memesKenya/meme/controller/AuthController.java
@@ -4,10 +4,14 @@ import com.memesKenya.meme.model.LoginRequest;
 import com.memesKenya.meme.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -27,6 +31,13 @@ public class AuthController {
         this.tokenService=tokenService;
     }
 
+    @PostMapping("/getAuthToken")
+    @ResponseBody
+    public String handleToken(@RequestBody String webToken) {
+        System.out.println("Cliked here and "+webToken);
+        return webToken;
+    }
+
     @PostMapping("/authenticate")
     public Map<String,String> token(@RequestBody LoginRequest loginRequest){
         System.out.println("Username "+loginRequest.username()+" password "+loginRequest.password());
@@ -35,11 +46,16 @@ public class AuthController {
             loginRequest.username(),
                     loginRequest.password()
         ));
-        System.out.println("passed to here");
         String token = tokenService.generateToken(authentication);
         Map<String,String> stringStringMap=new HashMap<>();
         stringStringMap.put("token",token);
         return stringStringMap;
     }
 
+    @GetMapping("/logged")
+    public String getAuthToken() {
+        String authToken="Suuceewfjb v";
+        System.out.println("name "+authToken);
+        return authToken;
+    }
 }
