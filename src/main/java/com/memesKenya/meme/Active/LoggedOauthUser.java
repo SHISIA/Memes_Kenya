81b54c18b59e241c512881cfe2f7e24fc6568d78
Jemo <jamesshisia6@gmail.com>
2023-09-29 20:04:01 +0300

    Changed LoggedUser to LoggedOauthUser, specifically for Logged users via Auth

---
+++ src/main/java/com/memesKenya/meme/Active/LoggedOauthUser.java
@@ -0,0 +1,16 @@
+package com.memesKenya.meme.Active;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@ToString
+public class LoggedOauthUser {
+    private String email;
+    private String password;
+}
