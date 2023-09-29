a140681e78ba75e2174176cd19d6097bd789e265
Jemo <jamesshisia6@gmail.com>
2023-09-29 18:52:28 +0300

    Code cleanup, removed circular dependencies

--- src/main/java/com/memesKenya/meme/service/_serviceImpls/MemerServiceImpl.java
+++ src/main/java/com/memesKenya/meme/service/_serviceImpls/MemerServiceImpl.java
@@ -1,6 +1,5 @@
 package com.memesKenya.meme.service._serviceImpls;
 
-import com.memesKenya.meme.controller.MemerController;
 import com.memesKenya.meme.entities.AccountStatus;
 import com.memesKenya.meme.entities.Authorities;
 import com.memesKenya.meme.entities.Memer;
@@ -11,33 +10,21 @@ import com.memesKenya.meme.model.Provider;
 import com.memesKenya.meme.repository.AuthoritiesRepo;
 import com.memesKenya.meme.repository.MemerRepo;
 import com.memesKenya.meme.repository.SecurityUserRepo;
-import com.memesKenya.meme.service.TokenService;
 import com.memesKenya.meme.service._service.MemerService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.http.*;
 import org.springframework.lang.NonNull;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -62,12 +49,9 @@ public class MemerServiceImpl implements MemerService {
     @Autowired
     private MemerRepo repo;
     private final PasswordEncoder passwordEncoder;
-    @Autowired
-    private TokenService tokenService;
     private Memer memer;
     private SecurityUser user;
     @Autowired
-    private AuthenticationManager authenticationManager;
     private Authorities authorities;
     public MemerServiceImpl(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
@@ -129,10 +113,9 @@ public class MemerServiceImpl implements MemerService {
         return repo.findByAnyName(name);
     }
 
-    public void processOAuthPostLogin(HttpServletResponse response,
-                                      HttpServletRequest request, Authentication authentication) throws IOException {
-        OAuth2User oauthUser = null;
-        String email = "",picture="",first_name="",sub="",last_name="",token="";
+    public void processOAuthPostLogin(HttpServletRequest request, Authentication authentication) throws IOException {
+        OAuth2User oauthUser;
+        String email = "",picture="",first_name="",sub="",last_name="";
         Provider provider=GOOGLE;
         if (request.getRequestURI().endsWith("google")){
             oauthUser = (DefaultOidcUser) authentication.getPrincipal();
@@ -142,7 +125,7 @@ public class MemerServiceImpl implements MemerService {
             first_name = oauthUser.getAttribute("given_name");
             last_name = oauthUser.getAttribute("family_name");
             sub = oauthUser.getAttribute("sub");
-//           Google uses DefaultOidcUser
+            //Google uses DefaultOidcUser
         }else if (request.getRequestURI().endsWith("github")){
             //GitHub uses OAuth2User
             provider=GITHUB;
@@ -184,75 +167,6 @@ public class MemerServiceImpl implements MemerService {
             newUser.setMemer(memer);
             newUser.setEnabled(1);
             securityUserRepo.save(newUser);
-            //authenticate after signing in using Oauth 2.0
-//            response.sendRedirect("/api/v1/auth/logged");
-        }
-        System.out.println(" token "+token);
-        response.sendRedirect("/api/v1/auth/logged");
-
-    }
-
-    public String getToken(String username,String password){
-        System.out.println("username "+username+"password "+password);
-        Authentication authentication= authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        username,
-                        password
-                ));
-       String token= tokenService.generateToken(authentication);
-        System.out.println("Token Successfully sent!!");
-        return token;
-    }
-
-    public String sendToken(String webToken) {
-        try {
-            // Create HttpHeaders with the Content-Type header and Authorization header
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.set("Authorization", "Bearer " + webToken);
-
-            // Create a request entity with the token as the payload and headers
-            HttpEntity<String> requestEntity = new HttpEntity<>(webToken, headers);
-
-            // Make an HTTP POST request to send the token to the API
-            ResponseEntity<String> responseEntity = restTemplate.postForEntity(apiUrl, requestEntity, String.class);
-
-            // Handle the API's response if needed
-            String response = responseEntity.getBody();
-            getToken(webToken);
-            return response;
-        } catch (HttpClientErrorException.Unauthorized e) {
-            // Handle the 401 Unauthorized error here
-            e.printStackTrace();
-            return "Authentication error";
-        }
-    }
-
-    public String getToken(String webToken){
-        try {
-            // Build the API endpoint URL with query parameters
-            UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(loggedUrl);
-                    // Add query parameters if needed
-//                    .queryParam("param1", "value1");
-            System.out.println("did iid ijee");
-            // Create HttpHeaders with the Authorization header
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", "Bearer " + webToken);
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            System.out.println("oijhvbwefwf");
-            // Make a GET request using getForEntity
-            ResponseEntity<String> responseEntity = restTemplate.exchange(builder.toUriString(),
-                    HttpMethod.GET,
-                    entity,
-                    String.class);
-
-            // Handle the API's response if needed
-            return responseEntity.getBody();
-        } catch (Exception e) {
-            // Handle errors here
-            e.printStackTrace();
-            return "Error occurred";
         }
     }
 
