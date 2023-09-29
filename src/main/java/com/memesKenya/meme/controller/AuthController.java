4eeb6ced4236a060323df4472395300e8128b0cb
Jemo <jamesshisia6@gmail.com>
2023-09-29 18:50:24 +0300

    Removed getAuthToken API endPoint used for testing

--- src/main/java/com/memesKenya/meme/controller/AuthController.java
+++ src/main/java/com/memesKenya/meme/controller/AuthController.java
@@ -2,16 +2,13 @@ package com.memesKenya.meme.controller;
 
 import com.memesKenya.meme.model.LoginRequest;
 import com.memesKenya.meme.service.TokenService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -31,13 +28,6 @@ public class AuthController {
         this.tokenService=tokenService;
     }
 
-    @PostMapping("/getAuthToken")
-    @ResponseBody
-    public String handleToken(@RequestBody String webToken) {
-        System.out.println("Cliked here and "+webToken);
-        return webToken;
-    }
-
     @PostMapping("/authenticate")
     public Map<String,String> token(@RequestBody LoginRequest loginRequest){
         System.out.println("Username "+loginRequest.username()+" password "+loginRequest.password());
@@ -52,10 +42,4 @@ public class AuthController {
         return stringStringMap;
     }
 
-    @GetMapping("/logged")
-    public String getAuthToken() {
-        String authToken="Suuceewfjb v";
-        System.out.println("name "+authToken);
-        return authToken;
-    }
 }
