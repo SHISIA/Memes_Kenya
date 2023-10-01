0ffa66a65bb4f49b097c0558467ab4fb7ab6be5e
Jemo <jamesshisia6@gmail.com>
2023-10-01 15:44:41 +0300

    Logged in user api model for public user info

--- src/main/java/com/memesKenya/meme/Active/LoggedOauthUser.java
+++ src/main/java/com/memesKenya/meme/Active/LoggedOauthUser.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 @ToString
 public class LoggedOauthUser {
-    private String email;
-    private String password;
+    private String userId;
+    private String nickName;
+    private String image;
 }
