c97b9753ea151cff70aeddca3a3ad0bc13b3f576
Jemo <jamesshisia6@gmail.com>
2023-10-31 01:54:48 +0300

    configure controller to accomodate message DTO

--- src/main/java/com/memesKenya/meme/service/_serviceImpls/MessageServiceImpl.java
+++ src/main/java/com/memesKenya/meme/service/_serviceImpls/MessageServiceImpl.java
@@ -2,10 +2,13 @@ package com.memesKenya.meme.service._serviceImpls;
 
 import com.memesKenya.meme.entities.Memer;
 import com.memesKenya.meme.entities.Message;
+import com.memesKenya.meme.messaging.SocketMessage;
 import com.memesKenya.meme.model.MessageModel;
 import com.memesKenya.meme.repository.MemerRepo;
 import com.memesKenya.meme.repository.MessageRepo;
+import com.memesKenya.meme.repository.SocketMessageRepo;
 import com.memesKenya.meme.service._service.MessageService;
+import com.memesKenya.meme.service._service.SocketMessageService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +20,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
-public class MessageServiceImpl implements MessageService {
+public class MessageServiceImpl implements MessageService, SocketMessageService {
     @Autowired
     private MessageRepo repo;
     @Autowired
     private MemerRepo memerRepo;
+
+    @Autowired
+    private SocketMessageRepo socketMessageRepo;
 
     @Override
     public List<Message> getMessagesByNickName(String nickName) {
@@ -38,6 +44,32 @@ public class MessageServiceImpl implements MessageService {
         repo.save(message_);
 //        repo.updateSenderAndReceiver(sender.getUserId(),recipient.getUserId(),message_.getMessageId());
         return "Message Sent";
+    }
+
+    //getMessageBySender - For SocketMessage service
+    @Override
+    public List<SocketMessage> getMessagesBySenderAndReceiver(UUID sender, UUID receiver) {
+        return socketMessageRepo.getMessagesBySenderAndReceiver(sender,receiver);
+    }
+
+    //Send - For socketMessage service
+    @Override
+    public SocketMessage sendMessage(SocketMessage message) {
+        socketMessageRepo.save(message);
+        return message;
+    }
+
+    //Delete - For socketMessage service
+    @Override
+    public boolean deleteSocketMessage(UUID sender, UUID receiver) {
+        socketMessageRepo.deleteSocketMessage(sender,receiver);
+        return false;
+    }
+
+    //getSocketMessage - For socketMessage service
+    @Override
+    public Optional<List<SocketMessage>> getSocketMessagesBySender(UUID senderId) {
+        return socketMessageRepo.getSocketMessagesBySender(senderId);
     }
 
     @Override
